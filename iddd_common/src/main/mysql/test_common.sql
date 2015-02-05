328d69655bfe1ba7cb0e05ca241ccf59b61e7a6b
laungcisin@qq.com <Zfl05975692379>
2015-02-05 19:41:16 +0800

    格式化代码。

--- iddd_common/src/main/mysql/test_common.sql
+++ iddd_common/src/main/mysql/test_common.sql
@@ -1,20 +1,21 @@
 DROP DATABASE IF EXISTS iddd_common_test;
 CREATE DATABASE iddd_common_test;
 USE iddd_common_test;
-SET FOREIGN_KEY_CHECKS=0;
+SET FOREIGN_KEY_CHECKS = 0;
 
 DROP TABLE IF EXISTS tbl_testable_time_constrained_process;
 CREATE TABLE `tbl_testable_time_constrained_process` (
-    `id` bigint(20) NOT NULL auto_increment,
-    `allowable_duration` bigint(20) NOT NULL,
-    `confirm1` tinyint(1) NOT NULL,
-    `confirm2` tinyint(1) NOT NULL,
-    `description` varchar(200),
-    `process_id_id` varchar(36) NOT NULL,
-    `process_completion_type` varchar(50) NOT NULL,
-    `start_time` datetime NOT NULL,
-    `tenant_id` varchar(36) NOT NULL,
-    `timed_out_date` datetime,
-    `concurrency_version` int(11) NOT NULL,
-    PRIMARY KEY (`id`)
-) ENGINE=InnoDB;
+  `id`                      BIGINT(20)  NOT NULL AUTO_INCREMENT,
+  `allowable_duration`      BIGINT(20)  NOT NULL,
+  `confirm1`                TINYINT(1)  NOT NULL,
+  `confirm2`                TINYINT(1)  NOT NULL,
+  `description`             VARCHAR(200),
+  `process_id_id`           VARCHAR(36) NOT NULL,
+  `process_completion_type` VARCHAR(50) NOT NULL,
+  `start_time`              DATETIME    NOT NULL,
+  `tenant_id`               VARCHAR(36) NOT NULL,
+  `timed_out_date`          DATETIME,
+  `concurrency_version`     INT(11)     NOT NULL,
+  PRIMARY KEY (`id`)
+)
+  ENGINE = InnoDB;
