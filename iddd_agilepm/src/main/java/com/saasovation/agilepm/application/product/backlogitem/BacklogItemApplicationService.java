b084ed2e8590fa4ac2d67966505a2130c38b9d59
laungcisin@qq.com <Zfl05975692379>
2015-02-13 15:39:12 +0800

    类添加注释。

--- iddd_agilepm/src/main/java/com/saasovation/agilepm/application/product/backlogitem/BacklogItemApplicationService.java
+++ iddd_agilepm/src/main/java/com/saasovation/agilepm/application/product/backlogitem/BacklogItemApplicationService.java
@@ -14,7 +14,15 @@
 
 package com.saasovation.agilepm.application.product.backlogitem;
 
+import com.saasovation.agilepm.domain.model.product.backlogitem.BacklogItem;
+import com.saasovation.agilepm.domain.model.product.backlogitem.BacklogItemCommitted;
+import com.saasovation.agilepm.domain.model.product.backlogitem.BacklogItemId;
 import com.saasovation.agilepm.domain.model.product.backlogitem.BacklogItemRepository;
+import com.saasovation.agilepm.domain.model.product.sprint.Sprint;
+import com.saasovation.agilepm.domain.model.product.sprint.SprintId;
+import com.saasovation.agilepm.domain.model.tenant.TenantId;
+import com.saasovation.common.domain.model.DomainEventPublisher;
+import com.saasovation.common.domain.model.DomainEventSubscriber;
 
 public class BacklogItemApplicationService {
 
@@ -28,6 +36,28 @@ public class BacklogItemApplicationService {
     }
 
     // TODO: APIs for student assignment
+    public void commitBacklogItem(TenantId tenantId,
+                                  BacklogItemId backlogItemId,
+                                  SprintId sprintId) {
+        DomainEventSubscriber subscriber =
+                new DomainEventSubscriber<BacklogItemCommitted>() {
+                    @Override
+                    public void handleEvent(BacklogItemCommitted aDomainEvent) {
+                        //TODO:这里处理事件。
+                    }
+
+                    @Override
+                    public Class<BacklogItemCommitted> subscribedToEventType() {
+                        return BacklogItemCommitted.class;
+                    }
+                };
+
+        DomainEventPublisher.instance().subscribe(subscriber);
+
+        BacklogItem backlogItem = this.backlogItemRepository.backlogItemOfId(tenantId, backlogItemId);
+
+        //FIXME:
+    }
 
     public void backlogItemPlaceholderService() {
         this.backlogItemRepository();
